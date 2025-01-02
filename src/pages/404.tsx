a3820a289d53f8b5a27d9bddca6854eb372ffd45
daniel_eln <omoniyiopemipo6@gmail.com>
2025-01-02 01:54:39 +0100

    Prepared basic app layout, fonts and assests of login page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div>
+            Couldn't find the page you're looking for
+            <button>Go back!</button>
+        </div>
+    );
+}
+
+export default PageNotFound;
