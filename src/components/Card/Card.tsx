dad203776ba6f59519a1a4952d4ac84602449561
daniel_eln <omoniyiopemipo6@gmail.com>
2025-01-03 14:01:53 +0100

    feat(user-data-cards): built dashboard  data cards

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+const Card = (props: {imgSrc: string, title: string, numbers: string}) => {
+    return (
+        <div className='card'>
+            <img src={props.imgSrc} alt='card icon'/>
+            <p>{props.title}</p>
+            <h3>{props.numbers}</h3>
+        </div>
+    );
+}
+
+export default Card;
--- src/pages/dashboard/pages/users.tsx
+++ src/pages/dashboard/pages/users.tsx
@@ -1,8 +1,39 @@
+import Card from "../../../components/Card/Card";
 
 const Users = () => {
+    const cardData = [
+        {
+            imgSrc: "../../../../public/users-images/users-icon.svg",
+            title: "USERS",
+            numbers: "2,453"
+        },
+        {
+            imgSrc: "../../../../public/users-images/active-users-icon.svg",
+            title: "ACTIVE USERS",
+            numbers: "2,453"
+        }, 
+        {
+            imgSrc: "../../../../public/users-images/users-with-loans-icon.svg",
+            title: "USERS WITH LOANS",
+            numbers: "12,453"
+        },
+        {
+            imgSrc: "../../../../public/users-images/users-with-savings.svg",
+            title: "USERS WITH SAVINGS",
+            numbers: "102,453"
+        }
+    ]
+
     return (
-        <div>
-            {/* <h1 style={{fontSize: "100rem", color: "black"}}>Users</h1> */}
+        <div id="users-page">
+            {/* <h1>HELLO THERE</h1> */}
+            <h2>Users</h2>
+
+            <div id="cards">
+                {cardData.map((data, index):any => <Card key={index} imgSrc={data.imgSrc} title={data.title} numbers={data.numbers} />)}
+            </div>
+
+            <div id="user-listing"></div>
         </div>
     );
 }
